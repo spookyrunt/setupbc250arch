abba8ae61ef483794406756b625f26b4d3c2f8b6
spookyrunt <283396452+spookyrunt@users.noreply.github.com>
2026-09-14 20:38:57 +0900

    Update llmtune installation path and symlinks

--- setup-llmtune.sh
+++ setup-llmtune.sh
@@ -10,9 +10,9 @@ sudo pacman -S --needed --noconfirm vulkan-tools mesa-utils
 sudo pacman -S --needed --noconfirm python-huggingface-hub
 
 # llmtune
-[ -d llmtune ] || git clone https://github.com/cachenetics/llmtune
+[ -d build/llmtune ] || git clone https://github.com/cachenetics/llmtune
 (
-  cd llmtune
+  cd build/llmtune
   ./install.sh # cargo build --release, installs to /usr/local/bin
 )
 sudo pacman -S --needed --noconfirm base-devel cmake git vulkan-headers vulkan-icd-loader spirv-headers shaderc vulkan-radeon
@@ -22,4 +22,6 @@ llmtune build install prism-vulkan
 
 # link
 sudo ln -s /var/lib/llmtune/src/prism-vulkan/build/bin/llama-server /usr/local/bin/llama-server
-# sudo ln -s /var/lib/llmtune/src/vulkan/build/bin/llama-server /usr/local/bin/llama-server
+sudo ln -s /var/lib/llmtune/src/vulkan/build/bin/llama-server /usr/local/bin/prism-llama-server
+sudo ln -s /var/lib/llmtune/models ~/models
+sudo ln -s /.config/llmtune ~/config
