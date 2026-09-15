7938a3eef03758d97c8f95c671f98f68278d77b5
u <>
2026-09-16 02:56:54 +0900

    minor fix

--- setup-llmtune.sh
+++ setup-llmtune.sh
@@ -24,8 +24,8 @@ sudo llmtune build install prism-vulkan
 # link
 [ -f /usr/local/bin/llama-server ] || sudo ln -s /var/lib/llmtune/src/vulkan/build/bin/llama-server /usr/local/bin/llama-server
 [ -f /usr/local/bin/prism-llama-server ] || sudo ln -s /var/lib/llmtune/src/prism-vulkan/build/bin/llama-server /usr/local/bin/prism-llama-server
-[ -d ~/models ] || sudo ln -s /var/lib/llmtune/models ~/models
-[ -d ~/config ] || sudo ln -s ~/.config/llmtune ~/config
+[ -d ~/models ] || ln -s /var/lib/llmtune/models ~/models
+[ -d ~/config ] || ln -s ~/.config/llmtune ~/config
 (
   cd ~/.config/llmtune
   [ -f profile.toml ] && mv profile.toml profile.toml.bak
